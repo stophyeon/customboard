f940ad9fa08a50a199f616941922326b0fbae633
stophyeon <jeongjihyeon401@gmail.com>
2024-01-09 17:57:06 +0900

    init

---
+++ src/main/java/com/example/customboard/CustomboardApplication.java
@@ -0,0 +1,13 @@
+package com.example.customboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/customboard/CustomboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.customboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
